f3ea6ce6e7c0c7a3b62b83e2a360c12eed74dc78
Jinming YAO <cwlmyjm@126.com>
2018-12-03 10:41:09 +0800

    Add Initial Code
    Add Initial Code

---
+++ openvr_treadmill/openvr_treadmill/openvr_treadmill.cpp
@@ -0,0 +1,27 @@
+﻿// openvr_treadmill.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include <openvr_driver.h>
+#include <easylogging++.h>
+
+INITIALIZE_EASYLOGGINGPP
+
+#define HMD_DLL_EXPORT extern "C" __declspec( dllexport )
+
+HMD_DLL_EXPORT void *HmdDriverFactory(const char *pInterfaceName, int *pReturnCode)
+{
+	if (0 == strcmp(vr::IServerTrackedDeviceProvider_Version, pInterfaceName))
+	{
+		return NULL;
+	}
+	else if (0 == strcmp(vr::IVRWatchdogProvider_Version, pInterfaceName))
+	{
+		return NULL;
+	}
+
+	if (pReturnCode)
+		*pReturnCode = vr::VRInitError_Init_InterfaceNotFound;
+
+	return NULL;
+}
